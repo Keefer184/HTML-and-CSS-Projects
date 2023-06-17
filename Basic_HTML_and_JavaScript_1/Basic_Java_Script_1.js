51fdf60f347c23c74056f3d60b07042135195ac6
Keefer184 <136768491+Keefer184@users.noreply.github.com>
2023-06-17 13:32:45 -0400

    Added HTML and script files for Basic HTML and JS 1 Assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_Java_Script_1.js
@@ -0,0 +1,7 @@
+alert("Are you sure you want to be here?");
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
